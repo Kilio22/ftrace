ff4b46db8c6244a2322162ec7fe6e65d87eb554d
Kylian Balan <kylian.balan@epitech.eu>
2020-04-11 14:52:50 +0200

    changes on get_function_name, wa are just waiting for functionnal stack, adding toto source file

--- src/get_function_name.c
+++ src/get_function_name.c
@@ -7,22 +7,26 @@
 
 #include "ftrace.h"
 
-static char *make_function_name(void)
+static char *make_function_name(unsigned long addr, char *lib_name)
 {
-    return strdup("G PAS DE NOM LOL");
+    char fct_name[50] = {'\0'};
+
+    sprintf(fct_name, "func_%#lx@%s\n", addr, lib_name);
+    return strdup(fct_name);
 }
 
 char *get_function_name(struct elf_file_s *elf, unsigned long addr)
 {
     GElf_Sym sym;
     size_t symbol_nb;
-    
-    symbol_nb = shdr.sh_size / shdr.sh_entsize;
+
+    if (elf->sym_shdr == NULL || elf->sym_data == NULL)
+        return make_function_name(addr, "toto");
+    symbol_nb = elf->sym_shdr->sh_size / elf->sym_shdr->sh_entsize;
     for (size_t i = 0; i < symbol_nb; ++i) {
         gelf_getsym(elf->sym_data, i, &sym);
         if (sym.st_value == addr && ELF64_ST_BIND(sym.st_info) != STB_LOCAL)
-            printf("%s\n", elf_strptr(elf, shdr.sh_link, sym.st_name));
+            return elf_strptr(elf->elf, elf->sym_shdr->sh_link, sym.st_name);
     }
-    elf_end(elf);
-    return NULL;
+    return make_function_name(addr, "toto");
 }
---
+++ toto.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2020
+** bootstrap
+** File description:
+** toto
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+// call   0x401146 <toto>
+// 0x401176 <main+9>:   call   0x401157 <tutu>
+
+void toto(void)
+{
+    printf("i am in toto()\n");
+}
+
+void tutu(void)
+{
+    printf("i am in tutu()");
+}
+
+int main(void)
+{
+    toto();
+    tutu();
+    exit(0);
+}
