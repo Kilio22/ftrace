c10a557e5f779d50265ecc30c9a48a181a6ea948
Thomas Bastien <thomas1.bastien@epitech.eu>
2020-04-11 20:00:21 +0200

    Fix: remove signals not working

--- src/signals.c
+++ src/signals.c
@@ -9,17 +9,7 @@
 
 int detect_signal(int wstatus, ftrace_t *ftrace)
 {
-    if (wstatus >> 8 == (SIGTRAP | (PTRACE_EVENT_EXIT << 8)))
-        handle_end_of_prog(ftrace, wstatus);
-    else if (WIFSIGNALED(wstatus)) {
-        print_signal(WTERMSIG(wstatus), ftrace);
-        return (1);
-    }
-    else if (WIFSTOPPED(wstatus)) {
-        if (WSTOPSIG(wstatus) != SIGTRAP && WSTOPSIG(wstatus) != SIGSTOP) {
-	        print_signal(WSTOPSIG(wstatus), ftrace);
-            return (1);
-        }
-    }
+    (void)wstatus;
+    (void)ftrace;
     return (0);
 }
