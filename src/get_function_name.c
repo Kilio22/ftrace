cad39b4872b56534b5e1d8bf09883b88ab606c8a
Thomas Bastien <thomas1.bastien@epitech.eu>
2020-04-11 14:25:07 +0200

    Fix: change get func name

--- src/get_function_name.c
+++ src/get_function_name.c
@@ -7,27 +7,32 @@
 
 #include "ftrace.h"
 
-static char *make_function_name()
+static char *make_function_name(void)
 {
     return strdup("G PAS DE NOM LOL");
 }
 
 char *get_function_name(struct elf_file_s *elf, unsigned long addr)
 {
-    // uint64_t base_offset = elf_getbase(elf->elf);
-    // GElf_Sym sym;
-    // char *strings;
-    // size_t count;
+    Elf_Scn *scn = NULL;
+    GElf_Shdr shdr;
+    Elf_Data *data;
+    GElf_Sym sym;
+    size_t symbol_nb;
 
-    // if (elf->sym_shdr == NULL)
-    //     return make_function_name();
-    // count = elf->sym_shdr->sh_size / elf->sym_shdr->sh_entsize;
-    // strings = base_offset + (shd[shd[i].sh_link]).sh_offset;
-    // for (size_t i = 0; i < count; ++i) {
-    //     gelf_getsym(elf->sym_data, i, &sym);
-    //     if (sym.st_value == addr) {
-    //         printf("Found symbol\n");
-    //     }
-    // }
+    while ((scn = elf_nextscn(elf, scn)) != NULL) {
+        gelf_getshdr(scn, &shdr);
+        if (shdr.sh_type == SHT_SYMTAB) {
+            break;
+        }
+    }
+    data = elf_getdata(scn, NULL);
+    symbol_nb = shdr.sh_size / shdr.sh_entsize;
+    for (size_t i = 0; i < count; ++i) {
+        gelf_getsym(elf->sym_data, i, &sym);
+        if (sym.st_value == offset && ELF64_ST_BIND(sym.st_info) != STB_LOCAL)
+            printf("%s\n", elf_strptr(elf, shdr.sh_link, sym.st_name));
+    }
+    elf_end(elf);
     return NULL;
 }
