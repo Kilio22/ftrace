6ed40ce99f859d403396de1cd11ed533d72aae50
Thomas Bastien <thomas1.bastien@epitech.eu>
2020-04-11 14:30:11 +0200

    Fix: get_func_name

--- src/get_function_name.c
+++ src/get_function_name.c
@@ -14,23 +14,12 @@ static char *make_function_name(void)
 
 char *get_function_name(struct elf_file_s *elf, unsigned long addr)
 {
-    Elf_Scn *scn = NULL;
-    GElf_Shdr shdr;
-    Elf_Data *data;
-    GElf_Sym sym;
     size_t symbol_nb;
-
-    while ((scn = elf_nextscn(elf, scn)) != NULL) {
-        gelf_getshdr(scn, &shdr);
-        if (shdr.sh_type == SHT_SYMTAB) {
-            break;
-        }
-    }
-    data = elf_getdata(scn, NULL);
+    
     symbol_nb = shdr.sh_size / shdr.sh_entsize;
-    for (size_t i = 0; i < count; ++i) {
+    for (size_t i = 0; i < symbol_nb; ++i) {
         gelf_getsym(elf->sym_data, i, &sym);
-        if (sym.st_value == offset && ELF64_ST_BIND(sym.st_info) != STB_LOCAL)
+        if (sym.st_value == addr && ELF64_ST_BIND(sym.st_info) != STB_LOCAL)
             printf("%s\n", elf_strptr(elf, shdr.sh_link, sym.st_name));
     }
     elf_end(elf);
