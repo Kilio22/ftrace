fb6a71376ebd26c164b9c7be869c55d87bfc9b02
Thomas Bastien <thomas1.bastien@epitech.eu>
2020-04-11 20:13:40 +0200

    Fix: signals working now with PTRACE_GETSINGINFO

--- src/signals.c
+++ src/signals.c
@@ -9,7 +9,11 @@
 
 int detect_signal(int wstatus, ftrace_t *ftrace)
 {
-    (void)wstatus;
-    (void)ftrace;
+    siginfo_t signal;
+
+    if (wstatus >> 8 == (SIGTRAP | (PTRACE_EVENT_EXIT << 8)))
+        handle_end_of_prog(ftrace, wstatus);
+    else if (ptrace(PTRACE_GETSIGINFO, ftrace->pid, 0, &signal) >= 0 && signal.si_signo != SIGTRAP && signal.si_signo != SIGSTOP)
+        print_signal(signal.si_signo, ftrace);
     return (0);
 }
